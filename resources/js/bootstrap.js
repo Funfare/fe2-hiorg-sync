7d7106857a5a431c26f4550264062ea37d9a0db9
Fabio Thoma <fabio.thoma@dream-soft.de>
2023-12-23 13:39:54 +0100

    [Feature] #xx: installed bootstrap local and embed via vite

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -6,8 +6,12 @@
 
 import axios from 'axios';
 window.axios = axios;
+import * as Popper from '@popperjs/core'
+window.Popper = Popper
+import 'bootstrap'
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,8 +7,8 @@
     <meta name="description" content="">
     <meta name="author" content="Nicholas Sterk">
     <title>FE2 - Hiorg Synchronisation</title>
-    <!-- Bootstrap core CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- Application FrontEnd Assets -->
+    @vite(['resources/js/app.js', 'resources/css/app.scss'])
     <!-- Favicons -->
     <meta name="theme-color" content="#7952b3">
 </head>
@@ -47,7 +47,5 @@
     </div>
 </main>
 
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
