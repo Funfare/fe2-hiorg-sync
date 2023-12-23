6ed71922e2dbc296f170ba6e8c1d9c537c5bc3fb
Fabio Thoma <fabio.thoma@dream-soft.de>
2023-12-23 14:54:30 +0100

    [Feature] #xx: facelifted master and me blade

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -23,19 +23,31 @@
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <ul class="navbar-nav me-auto mb-2 mb-md-0">
                 <li class="nav-item">
-                    <a class="nav-link {{ Route::is('home') ? 'active' : '' }}" aria-current="page" href="{{route('home')}}">Home</a>
+                    <a class="nav-link {{ Route::is('home') ? 'active' : '' }}" aria-current="page" href="{{route('home')}}">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="pb-1" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l-2 0l9 -9l9 9l-2 0" /><path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" /><path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" /></svg>
+                        Home
+                    </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link {{ Route::is('me') ? 'active' : '' }}" aria-current="page" href="{{route('me')}}">Meine Daten</a>
+                    <a class="nav-link {{ Route::is('me') ? 'active' : '' }}" aria-current="page" href="{{route('me')}}">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="pb-1" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 6m-8 0a8 3 0 1 0 16 0a8 3 0 1 0 -16 0" /><path d="M4 6v6a8 3 0 0 0 16 0v-6" /><path d="M4 12v6a8 3 0 0 0 16 0v-6" /></svg>
+                        Meine Daten
+                    </a>
                 </li>
                 @can('admin')
                 <li class="nav-item">
-                    <a class="nav-link {{ Route::is('settings') ? 'active' : '' }}" href="{{route('settings')}}">Einstellungen</a>
+                    <a class="nav-link {{ Route::is('settings') ? 'active' : '' }}" href="{{route('settings')}}">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="pb-1" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z" /><path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" /></svg>
+                        Einstellungen
+                    </a>
                 </li>
                 @endcan
             </ul>
             <div class="d-flex">
-                <a href="{{ route('logout') }}" class="btn btn-outline-secondary">Logout</a>
+                <a href="{{ route('logout') }}" class="btn btn-outline-light">
+                    Abmelden
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 8v-2a2 2 0 0 1 2 -2h7a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-7a2 2 0 0 1 -2 -2v-2" /><path d="M15 12h-12l3 -3" /><path d="M6 15l-3 -3" /></svg>
+                </a>
             </div>
         </div>
     </div>
--- resources/views/organizations/me.blade.php
+++ resources/views/organizations/me.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.master')
 
 @section('content')
+    <div class="d-inline">
+
+    </div>
     <h2>{{ Auth::user()->organization->name }}</h2>
 
-    <a href="{{ route('home') }}" class="btn btn-secondary pb-2">Zurück</a>
     @if(!$valid)
         <div class="alert alert-danger" role="alert">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
@@ -12,46 +14,68 @@
                 <path
                     d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
             </svg>
-            Es erfolgt keine Synchronisierung zum aPager und keine Alarmierung!
+            Es erfolgt derzeit keine Synchronisierung zum aPager und keine Alarmierung!
         </div>
-        <b>Bitte überprüfe deine Zugehörgkeit zur Gruppe "Handyalarmierung" und dein Geburtsdatum</b> <br />
-        Änderungen kannst du über das 4JUH Formular <a href="https://www.4juh.de/workspaces/sanitaetsbereitschaft-wuerzburg/apps/form/zugaenge-edv-programme" target="_blank">Zugänge IT/Programme</a> beantragen.
+        <b>Bitte überprüfe deine Zugehörgkeit zur Gruppe "Handyalarmierung" und dein Geburtsdatum.</b> <br />
+        Änderungen kannst du über das 4JUH Formular <a href="https://www.4juh.de/workspaces/sanitaetsbereitschaft-wuerzburg/apps/form/zugaenge-edv-programme" target="_blank">Zugänge IT/Programme</a> beantragen.<br /><br />
     @else
 <p>
-    Diese Daten werden automatisch übertragen. <br />
-    Fehler können über <a href="https://www.4juh.de/workspaces/sanitaetsbereitschaft-wuerzburg/apps/form/zugaenge-edv-programme" target="_blank">4JUH Zugänge IT/Programme</a> gemeldet werden.
+    Die folgenden Daten werden automatisch übertragen. Fehler können über <a href="https://www.4juh.de/workspaces/sanitaetsbereitschaft-wuerzburg/apps/form/zugaenge-edv-programme" target="_blank">4JUH Zugänge IT/Programme</a> gemeldet werden.
 </p>
         <table class="table table-striped">
             <tr>
-                <td>Name</td>
+                <td>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" /><path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /></svg>
+                    Name
+                </td>
                 <td> {{ $fe2['firstName'] }} {{ $fe2['lastName'] }}</td>
             </tr>
             <tr>
-                <td>E-Mail</td>
+                <td>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z" /><path d="M3 7l9 6l9 -6" /></svg>
+                    E-Mail
+                </td>
                 <td> {{ $fe2['email'] }}</td>
             </tr>
             <tr>
-                <td>aPagerPro Adresse</td>
+                <td>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M22 7.535v9.465a3 3 0 0 1 -2.824 2.995l-.176 .005h-14a3 3 0 0 1 -2.995 -2.824l-.005 -.176v-9.465l9.445 6.297l.116 .066a1 1 0 0 0 .878 0l.116 -.066l9.445 -6.297z" stroke-width="0" fill="currentColor" /><path d="M19 4c1.08 0 2.027 .57 2.555 1.427l-9.555 6.37l-9.555 -6.37a2.999 2.999 0 0 1 2.354 -1.42l.201 -.007h14z" stroke-width="0" fill="currentColor" /></svg>
+                    aPagerPro Adresse
+                </td>
                 <td> {{ $fe2['aPagerPro'] }}</td>
             </tr>
             <tr>
-                <td>Telefonnummer</td>
+                <td>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" /><path d="M15 7a2 2 0 0 1 2 2" /><path d="M15 3a6 6 0 0 1 6 6" /></svg>
+                    Telefonnummer
+                </td>
                 <td> {{ $fe2['mobil'] }}</td>
             </tr>
             <tr>
-                <td>Bemerkung</td>
+                <td>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M13 20l7 -7" /><path d="M13 20v-6a1 1 0 0 1 1 -1h6v-7a2 2 0 0 0 -2 -2h-12a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7" /></svg>
+                    Bemerkung
+                </td>
                 <td> {{ $fe2['note'] }}</td>
             </tr>
             <tr>
-                <td>Funktionen</td>
+                <td>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 13a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M8 21v-1a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2v1" /><path d="M15 5a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M17 10h2a2 2 0 0 1 2 2v1" /><path d="M5 5a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M3 13v-1a2 2 0 0 1 2 -2h2" /></svg>
+                    Funktionen
+                </td>
                 <td> {{ implode(', ', $fe2['osFunctions']) }}</td>
             </tr>
             <tr>
-                <td>Alarmgruppen</td>
+                <td>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 5a2 2 0 0 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6" /><path d="M9 17v1a3 3 0 0 0 6 0v-1" /><path d="M21 6.727a11.05 11.05 0 0 0 -2.794 -3.727" /><path d="M3 6.727a11.05 11.05 0 0 1 2.792 -3.727" /></svg>
+                    Alarmgruppen
+                </td>
                 <td> {{ implode(', ', $fe2['alarmGroups']) }}</td>
             </tr>
         </table>
     @endif
-    <p class="pt-3">Diese Daten werden für eine Stunde zwischengespeichert, eine Änderung im Hiorg-Server ist erst mit Verzögerung
-        sichtbar</p>
+    <p class="pt-3">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19.875 6.27c.7 .398 1.13 1.143 1.125 1.948v7.284c0 .809 -.443 1.555 -1.158 1.948l-6.75 4.27a2.269 2.269 0 0 1 -2.184 0l-6.75 -4.27a2.225 2.225 0 0 1 -1.158 -1.948v-7.285c0 -.809 .443 -1.554 1.158 -1.947l6.75 -3.98a2.33 2.33 0 0 1 2.25 0l6.75 3.98h-.033z" /><path d="M12 9h.01" /><path d="M11 12h1v4h1" /></svg>
+        Diese Daten werden für eine Stunde zwischengespeichert, eine Änderung im Hiorg-Server ist erst mit Verzögerung sichtbar.
+    </p>
 @endsection
